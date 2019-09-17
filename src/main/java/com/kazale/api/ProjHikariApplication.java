e68515790e3569a5c0687c3ab51535c8618160dc
velioterio <velioterio@dxc.com>
2019-09-17 09:24:15 -0300

    Subindo alterações simples feitas no projetinho do hikari, mexi nas properties

--- src/main/java/com/kazale/api/ProjHikariApplication.java
+++ src/main/java/com/kazale/api/ProjHikariApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class P18Application {
+public class ProjHikariApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(P18Application.class, args);
+		SpringApplication.run(ProjHikariApplication.class, args);
 	}
 }
